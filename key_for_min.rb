d8d0d1b53e9b558e2611c786f994c905cffb2f89
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-28 17:36:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value = 10000000
+  name_hash.each do |key, value|
+    if min_value > value
+       min_value = key
+    end
+   end
+   min_value
 end
